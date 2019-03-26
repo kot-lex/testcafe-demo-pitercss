7d1048e1355dd3e238ac45fa3c780331917921f1
Aleksei Chernov <kot.lex@gmail.com>
2019-03-26 13:15:41 +0300

    Add show more button test

--- test/mainPage.js
+++ test/mainPage.js
@@ -1,4 +1,19 @@
+import { Selector } from 'testcafe';
+
 fixture('Main Page')
     .page('https://wsd.events');
 
 test('Page opens successfully', async t => t);
+
+test('More events button changes amout of events', async t => {
+
+    const moreEventsButtonSelector = Selector('.calendar__button');
+    const eventsSelector = Selector('.calendar__item:not(.calendar__item--hidden) .calendar__event', { visibilityCheck: true });
+    
+    const eventsCount = await eventsSelector().count;
+    
+    await t.click(moreEventsButtonSelector);
+    const expanedEventsCount = await eventsSelector().count;
+
+    await t.expect(expanedEventsCount).gt(eventsCount);
+});
